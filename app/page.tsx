91610ad06cd0039ed75fca28d62fe5a2453b24ce
Hausel Kinga <hausel.kinga@gmail.com>
2023-07-29 21:44:55 +0200

    add intro

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-   <div>Hello World</div>
+    <div>
+      <h1>Hello, I am Kinga</h1>
+      <h2>Junior Full-Stack Developer</h2>
+      <p>In case You are looking for an enthusiastic junior developer, please don't hesitate to reach out to me!</p>
+    </div>
   )
 }
