adc0ab87bd9374852430ec67bf28547f0f5b62bf
Hausel Kinga <hausel.kinga@gmail.com>
2023-07-29 22:15:51 +0200

    add basic navbar and background image

---
+++ app/navbar.tsx
@@ -0,0 +1,14 @@
+import styles from './styles/navbar.module.css'
+
+export default function Navbar() {
+  return (
+    <div className={styles.container}>
+      <div>Kinga Hausel</div>
+      <div>
+        <a href="#">About</a>
+        <a href="#">Skills</a>
+        <a href="#">Contact</a>
+      </div>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import styles from './styles/page.module.css'
+
 export default function Home() {
   return (
-    <div>
+    <div className={styles.center}>
       <h1>Hello, I am Kinga</h1>
       <h2>Junior Full-Stack Developer</h2>
       <p>In case You are looking for an enthusiastic junior developer, please don't hesitate to reach out to me!</p>
