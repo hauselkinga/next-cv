d55bfb210557f2dc7218ca2dffae0f5bc026fe5c
Hausel Kinga <hausel.kinga@gmail.com>
2023-08-02 09:56:02 +0200

    clean up contacts

--- app/comps/text.tsx
+++ app/comps/text.tsx
@@ -1,39 +1,46 @@
-"use client"
-import styles from '../styles/text.module.css'
-import { useEffect } from 'react'
+"use client";
+import styles from "../styles/text.module.css";
+import { useEffect } from "react";
 
 export default function Text() {
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  })
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
 
   const handleScroll = (e: Event) => {
-    const element = (e.target as Document).getElementById('text-container');
-    
+    const element = (e.target as Document).getElementById("text-container");
+
     if (element && scrollY > screen.height) {
-      element.innerHTML = `
-      <i class="fa-brands fa-linkedin"></i>
-      <i class="fa-brands fa-square-github"></i>
-      <i class="fa-solid fa-envelope"></i>
-      `
-    } else if(element) {
-      element.innerHTML =
-      `<div>
-        <h1>Hello I am Kinga</h1>
-        <h2>Junior Full-Stack Developer</h2>
-      </div>`
+      (element.firstChild as HTMLElement).classList.add("hide");
+      (element.lastChild as HTMLElement).classList.remove("hide");
+    } else if (element) {
+      (element.firstChild as HTMLElement).classList.remove("hide");
+      (element.lastChild as HTMLElement).classList.add("hide");
     }
-  }
+  };
 
   return (
-    <div id='text-container' className={styles.container}>
-      <div>
-        <h1>Hello I am Kinga</h1>
-        <h2>Junior Full-Stack Developer</h2>
-      </div>
+    <div id="text-container" className={styles.container}>
+      <section>
+          <h1>Hello I am Kinga</h1>
+          <h2>Junior Full-Stack Developer</h2>
+      </section>
+      <section className="hide">
+        <div>
+          <a href="https://www.linkedin.com/in/kinga-hausel/">
+            <i className="fa-brands fa-linkedin"></i>
+          </a>
+          <a href="https://github.com/hauselkinga">
+            <i className="fa-brands fa-square-github"></i>
+          </a>
+          <a href="mailto:hausel.kinga@gmail.com">
+            <i className="fa-solid fa-envelope"></i>
+          </a>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
